4de154b4b03867d9b8eb48f06314e9adbd210e47
Fabio-Leonardo <fabiog144@gmail.com>
2026-06-14 22:46:54 -0300

    add myinforPage.js update

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -2,60 +2,32 @@ import userData from '../fixtures/users/userData.json'
 import LoginPage from '../pages/loginPage'
 import DashboardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
+import MyinforPage from '../pages/myinforPage'
+
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
+const myInfoPage = new MyinforPage()
 
-describe('Orange HRM Tests', () => {
-  const selectorsList = {
-    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb > .oxd-text',
-    dashboradGrid: '.orangehrm-dashboard-grid',
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
-    firstNameField: '[name="firstName"]',
-    lastNameField: '[name="lastName"]',
-    middleNameField: '[name="middleName"]',
-    genericField: '.oxd-input--active',
-    dateField: '[placeholder="yyyy-dd-mm"]',
-    listItem: '[tabindex="0"]',
-    listMaritalStatus: '.oxd-select-wrapper',
-    CloseButton: ".--close",
-    submitButton: '[type="submit"]',
-
-
-  }
-
-
+describe('User Infor Update', () => { 
 
   it.only('User Infor Update - success', () => {
     loginPage.accessLoginPage()
     loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
     dashboardPage.checkDashboardPage()
     menuPage.acessMyInfo()
-    cy.get(selectorsList.firstNameField).clear().type('Jose')
-    cy.get(selectorsList.middleNameField).clear().type('das')
-    cy.get(selectorsList.lastNameField).clear().type('Botas')
-    cy.get(selectorsList.genericField).eq(3).clear().type('Zé das Botas')
-    cy.get(selectorsList.genericField).eq(3).clear().type('1234567890')
-    cy.get(selectorsList.genericField).eq(4).clear().type('OtherID123')
-    cy.get(selectorsList.genericField).eq(5).clear().type('DriverLicense123')
-    cy.get(selectorsList.genericField).eq(7).clear().type('ssnNumber123')
-    cy.get(selectorsList.genericField).eq(8).clear().type('sinNumber123')
-    cy.get(selectorsList.dateField).eq(0).clear().type('2036-11-11')
-    cy.get(selectorsList.CloseButton).click()
-    cy.get(selectorsList.dateField).eq(1).clear().type('2023-11-11')
-    cy.get(selectorsList.CloseButton).click()
-    cy.get(selectorsList.genericField).eq(11).clear().type('MilitaryService')
-    cy.get(selectorsList.listItem).eq(0).click()
-    cy.get('.oxd-select-dropdown > :nth-child(27)').click()
-    cy.get(selectorsList.listMaritalStatus).eq(1).click()
-    cy.get('.oxd-select-dropdown > :nth-child(3)').click()
-    cy.get(selectorsList.submitButton).eq(0).click()
-    cy.get('body').should('contain', 'Successfully Updated')
-    cy.get('.oxd-toast-close').click()
+    myInfoPage.fillPersonalDetails("John", "Doe", "Smith")
+    myInfoPage.fillOtherDetails("John Doe", "EMP001", "DL001", "SSN001", "SIN001")
+    myInfoPage.fillDateOfField("2020-01-03")
+    myInfoPage.fillDateOfBirth("1990-01-01")
+    myInfoPage.fillmaritalStatus("Married")
+    //myInfoPage.fillMilitaryService("No")
+    myInfoPage.fillNationality("27")
+    myInfoPage.fillListItem("1")
+    myInfoPage.submitForm()
+    myInfoPage.checkSuccessMessage()  
 
-
-
-
+   
   })
   it.skip('Login - Fail', () => {
     cy.visit('/auth/login')
---
+++ cypress/pages/myinforPage.js
@@ -0,0 +1,72 @@
+class MyinforPage {
+    selectorsList() {
+        const selectors = {
+            // Define selectors for the My Info page
+            firstNameField: '[name="firstName"]',
+            lastNameField: '[name="lastName"]',
+            middleNameField: '[name="middleName"]',
+            genericField: '.oxd-input--active',
+            dateField: '[placeholder="yyyy-dd-mm"]',
+            dateBirthField: '[placeholder="yyyy-dd-mm"]',
+            listItem: '[tabindex="0"]',
+            listMaritalStatus: '.oxd-select-wrapper',
+            CloseButton: ".--close",
+            submitButton: '[type="submit"]',
+            maritalStatus: '.oxd-select-dropdown > :nth-child(3)',
+            listNationality: '.oxd-select-wrapper',
+            nationality: '.oxd-select-dropdown > :nth-child(27)',
+            checkSuccessMessage: '.oxd-toast-content',  
+        }
+        return selectors;
+    }
+
+    fillPersonalDetails(firstName, middleName, lastName) {
+        cy.get(this.selectorsList().firstNameField).clear().type(firstName)
+        cy.get(this.selectorsList().middleNameField).clear().type(middleName)
+        cy.get(this.selectorsList().lastNameField).clear().type(lastName)
+              
+    }
+    fillOtherDetails(nickName, employeeId, driverLicense, ssnNumber, sinNumber) {
+        cy.get(this.selectorsList().genericField).eq(3).clear().type(nickName)
+        cy.get(this.selectorsList().genericField).eq(4).clear().type(employeeId)
+        cy.get(this.selectorsList().genericField).eq(5).clear().type(driverLicense)
+        cy.get(this.selectorsList().genericField).eq(7).clear().type(ssnNumber)
+        cy.get(this.selectorsList().genericField).eq(8).clear().type(sinNumber)
+    }
+    fillDateOfField(dateField) {
+        cy.get(this.selectorsList().dateField).eq(0).clear().type(dateField)
+        cy.get(this.selectorsList().CloseButton).click()
+    }
+    fillDateOfBirth(dateOfBirth) {
+        cy.get(this.selectorsList().dateBirthField).eq(1).clear().type(dateOfBirth)
+        cy.get(this.selectorsList().CloseButton).click()
+    
+    }
+    fillmaritalStatus(maritalStatus) {
+        cy.get(this.selectorsList().listMaritalStatus).eq(1).click()
+        cy.get(this.selectorsList().maritalStatus).click()
+    }
+    fillMilitaryService(militaryService) {
+        cy.get(this.selectorsList().genericField).eq(11).clear().type(militaryService)
+    }
+    fillNationality(nationality) {
+        cy.get(this.selectorsList().listNationality).eq(0).click()
+        cy.get(this.selectorsList().nationality).click()
+    }
+    fillListItem(listItem) {
+        cy.get(this.selectorsList().listItem).eq(0).click()
+    }
+    submitForm() {
+        cy.get(this.selectorsList().submitButton).eq(0).click()
+    }
+    checkSuccessMessage() {
+        cy.get(this.selectorsList().checkSuccessMessage).should('contain', 'Successfully Updated')
+        cy.get(this.selectorsList().CloseButton).click()
+    }   
+
+
+
+    
+
+}
+export default MyinforPage
